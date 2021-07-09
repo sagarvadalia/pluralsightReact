1f6f181cb8191aa07bfc7145be2b61333e782e6a
sagarvadalia <sv344@cornell.edu>
2021-07-09 16:05:51 -0400

    duplicate button components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import Button from './Components/Button';
 import Display from './Components/Display'
 import react, { useState } from 'react';
 function App() {
-  const [counter, setCounter] = useState(42)
-  const incrementCounter = () => setCounter(counter+1)
+  const [counter, setCounter] = useState(0)
+  const incrementBy1 = () => setCounter(counter + 1)
+  const incrementBy5 = () => setCounter(counter + 5)
+  const incrementBy10 = () => setCounter(counter + 10)
+  const incrementBy20 = () => setCounter(counter + 20)
   return (
     <div className="App">
       <header className="App-header">
+        <div class = "flex">
+          <Button onClickHandler={incrementBy1} increment = {1}></Button>
+        <Button onClickHandler={incrementBy5} increment = {5}></Button>
+        <Button onClickHandler={incrementBy10} increment = {10}></Button>
+        <Button onClickHandler = {incrementBy20} increment = {20}></Button>
+        </div>
 
-        <Button onClickHandler = {incrementCounter}></Button>
         <Display message={counter}></Display>
 
       </header>

4e92f0eccfce2de475cadde733946d0aaf83f0c8
sagarvadalia <sv344@cornell.edu>
2021-07-09 15:50:26 -0400

    pluralsight learning

---
+++ src/Components/Button.js
@@ -0,0 +1,9 @@
+
+import react, {useState} from 'react'
+
+function Button() {
+  const [counter, setCounter] = useState(0)
+
+  return <button onClick = {() => setCounter(counter+1)}> {counter} </button>
+}
+export default Button
